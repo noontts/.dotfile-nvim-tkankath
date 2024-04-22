0a652583e7549f22e6e08451cf679cd06cd0b56d
thitisan.k <thitisan.k@bluebik.com>
2024-04-22 15:23:18 +0700

    feat: update alpha.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -13,3 +13,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
+vim.keymap.set('n', '<leader>fa', '<Plug>(coc-codeaction-selected)')
+
+
